72ad90567273de5df4461cf10c6ebbff5292e453
NaotoNAKATA <nakatanaoto55@gmail.com>
2019-01-18 00:23:52 +0900

    Modify 03
    bug fix

--- 03_creating_package.sh
+++ 03_creating_package.sh
@@ -5,10 +5,14 @@
 #
 
 WORK=~/work/ros_tutorial
-cd ${WORK}/catkin_ws/src
+
+# 環境の設定
+source ${WORK}/catkin_ws/devel/setup.bash
+
+# 
+roscd
+cd ../src
 
 # std_msgs, roscpp, rospyに依存した新しいパッケージを'beginner_tutorials'を作成
 catkin_create_pkg beginner_tutorials std_msgs rospy roscpp
 
-# パッケージの依存
-rospack depends1 beginner_tutorials 
