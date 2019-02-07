f59140487077d7f21407024812a4d0f3aca25c2f
NaotoNAKATA <nakatanaoto55@gmail.com>
2019-02-07 10:57:09 +0900

    beginner_tutorialsにsrv/AddTwoInts.srvを追加 ビルド前
    roscpを使ってチュートリアルからコピーする

---
+++ 04_roscp_srv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# サービス(srv)を使う
+#
+#http://wiki.ros.org/ja/ROS/Tutorials/CreatingMsgAndSrv
+
+WORK=~/work/ros_tutorial
+
+# 環境の設定
+source ${WORK}/catkin_ws/devel/setup.bash
+
+# ディレクトリの移動 
+roscd beginner_tutorials
+
+# ディレクトリがなければ作成する
+if [ ! -e srv ] ; then
+	mkdir srv
+fi
+
+#rospy_tutorialsパッケージからサービスをコピー
+roscp rospy_tutorials AddTwoInts.srv srv/AddTwoInts.srv
+
