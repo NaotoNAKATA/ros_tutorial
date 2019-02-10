9ebcfbd83b84b6517bbdb28b9ee2c889e94c71ab
NaotoNAKATA <nakatanaoto55@gmail.com>
2019-02-10 18:22:02 +0900

    シンプルな配信者(Publisher)と購読者(Subscriber)の実行スクリプト
    引数でC++,Python,Publisher,Subscriberの切り替え

---
+++ 06_ros_Pub_Sub.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# シンプルな配信者(Publisher)と購読者(Subscriber)を書く(C++)
+#
+
+WORK=~/work/ros_tutorial
+
+# 環境の設定
+source ${WORK}/catkin_ws/devel/setup.bash
+
+# 
+if [ $1 -eq 0 ] ; then
+	# roscoreを起動しておく
+	roscore
+elif [ $1 -eq 1 ] ; then
+	# 配信者の起動(C++)
+	rosrun beginner_tutorials talker
+elif [ $1 -eq 2 ] ; then
+	# 購読者の起動(C++)
+	rosrun beginner_tutorials listener
+elif [ $1 -eq 3 ] ; then
+	# 配信者の起動(python)
+	rosrun beginner_tutorials talker.py
+elif [ $1 -eq 4 ] ; then
+	# 購読者の起動(python)
+	rosrun beginner_tutorials listener.py
+fi
+
+
