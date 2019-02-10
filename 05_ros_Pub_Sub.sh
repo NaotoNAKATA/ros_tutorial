962de3c8111aeb0d3c6638b53a1ec642295010f3
NaotoNAKATA <nakatanaoto55@gmail.com>
2019-02-10 18:18:14 +0900

    シンプルな配信者(Publisher)と購読者(Subscriber)を書く(python)
    OK

--- 05_ros_Pub_Sub.sh
+++ 05_ros_Pub_Sub.sh
@@ -16,3 +16,25 @@ roscd beginner_tutorials/src
 wget https://raw.github.com/ros/ros_tutorials/hydro-devel/roscpp_tutorials/talker/talker.cpp
 wget https://raw.github.com/ros/ros_tutorials/hydro-devel/roscpp_tutorials/listener/listener.cpp
 
+#
+# シンプルな配信者(Publisher)と購読者(Subscriber)を書く(python)
+#
+
+# ディレクトリの移動 
+roscd beginner_tutorials
+
+# ディレクトリがなければ作成する
+if [ ! -e scripts ] ; then
+	mkdir scripts
+fi
+cd scripts
+
+wget https://raw.github.com/ros/ros_tutorials/indigo-devel/rospy_tutorials/001_talker_listener/talker.py
+wget https://raw.github.com/ros/ros_tutorials/indigo-devel/rospy_tutorials/001_talker_listener/listener.py
+chmod +x talker.py
+chmod +x listener.py
+
+roscd
+cd ..
+catkin_make
+
